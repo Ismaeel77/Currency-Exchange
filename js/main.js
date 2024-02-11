ed4e8510ce585608390fd3235184f7078d826145
Ismaeel Abuzahra <abozahra.ismaeel@yahoo.com>
2024-02-11 06:55:49 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Open & Close Functios for the right box
+let selectBtn1 = document.querySelector(".dropdown .btn-1");
+let currencyList1 = document.querySelector(".dropdown .dropdown-menu .list-1");
+let options1 = currencyList1.querySelectorAll(".option");
+
+selectBtn1.addEventListener("click", () => {
+  currencyList1.classList.toggle("active");
+});
+
+options1.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    options1.forEach((opt) => {
+      opt.classList.remove("active");
+    });
+    if (!option.classList.contains("active")) {
+      e.target.classList.add("active");
+      selectBtn1.textContent = e.target.textContent;
+      currencyList1.classList.toggle("active");
+    }
+  });
+});
+
+// Open & Close Functios For The Left Box
+let selectBtn2 = document.querySelector(".dropdown .btn-2");
+let currencyList2 = document.querySelector(".dropdown .dropdown-menu .list-2");
+let options2 = currencyList2.querySelectorAll(".option");
+
+selectBtn2.addEventListener("click", () => {
+  currencyList2.classList.toggle("active");
+});
+
+options2.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    options2.forEach((opt) => {
+      opt.classList.remove("active");
+    });
+    if (!option.classList.contains("active")) {
+      e.target.classList.add("active");
+      selectBtn2.textContent = e.target.textContent;
+      currencyList2.classList.toggle("active");
+    }
+  });
+});
